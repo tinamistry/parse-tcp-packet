aa062a7be8934c8086ccada849b3d8d6a3dd0180
Tina Mistry <tmistry10@gmail.com>
2023-03-06 19:59:31 -0500

    got correct sequence and ack #s and the receive window size

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from dpkt.utils import inet_to_str
 
 def analysis_pcap_tcp(fileName):
     sender = '130.245.145.12'
-    receiver = ' 128.208.2.198'
+    receiver = '128.208.2.198'
     f = open('assignment2.pcap', 'rb')
     pcap = dpkt.pcap.Reader(f)
     flow_counter = 0
     setup = 0
+    count = 0
     for ts, buffer in pcap:
         eth = dpkt.ethernet.Ethernet(buffer)
         ip = eth.data
@@ -20,41 +21,58 @@ def analysis_pcap_tcp(fileName):
         source_ip = inet_to_str(ip.src)
         dest_ip = inet_to_str(ip.dst)
 
-        if source_ip == sender:
-            if tcp.flags & dpkt.tcp.TH_SYN:  # if the source is the sender its the beginning
-                flow_counter += 1
-                print('Flow #' + str(flow_counter))
-                print('Source IP: ' + str(source_ip) + " Source Port: " + str(source_port))
-                print('Destination IP: ' + str(dest_ip) + " Destination Port: " + str(dest_port))
-                first_two_transaction(source_ip, source_port, dest_port, dest_ip)
-
-        if dest_ip == sender:
-            if tcp.flags & dpkt.tcp.TH_FIN:
-                print(flow_counter)
+        if source_ip == sender and tcp.flags & dpkt.tcp.TH_SYN:  # if the source is the sender its the beginning
+            count += 1
+            flow_counter += 1
+            print('Flow #' + str(flow_counter))
+            print('Source IP: ' + str(source_ip) + " Source Port: " + str(source_port)
+                  + ' Destination IP: ' + str(dest_ip) + " Destination Port: " + str(dest_port))
+            t(source_ip, dest_ip, source_port, dest_port)
 
 
-def first_two_transaction(source_ip, source_port, dest_port, dest_ip):
+def t(sip, dip, sp, dp):
     sender = '130.245.145.12'
-    receiver = ' 128.208.2.198'
+    receiver = '128.208.2.198'
     f = open('assignment2.pcap', 'rb')
     pcap = dpkt.pcap.Reader(f)
-    transaction = 0
+    i = 0
+    j = 0
     for ts, buffer in pcap:
         eth = dpkt.ethernet.Ethernet(buffer)
         ip = eth.data
         tcp = ip.data
-        sp = tcp.sport  # source port
-        dp = tcp.dport  # destination port
-        sip = inet_to_str(ip.src)
-        dip = inet_to_str(ip.dst)
-        if transaction == 1 and source_ip == sender:
-            print("Transaction #1: Sequence #: " + str(tcp.seq) + " Ack #: " + str(tcp.ack))
-            transaction += 1
-        elif transaction == 2 and source_ip == sender:
-            print("Transaction #2: Sequence #: " + str(tcp.seq) + " Ack #: " + str(tcp.ack))
-            return
-        elif source_ip == sip and source_port == sp and dest_port == dp and dest_ip == dip:
-            transaction = 1
+        source_port = tcp.sport  # source port
+        dest_port = tcp.dport  # destination port
+        source_ip = inet_to_str(ip.src)
+        dest_ip = inet_to_str(ip.dst)
+
+        if source_ip == sip and dest_ip == dip and dest_port == dp and source_port == sp:
+            i += 1
+            if i > 2:
+                j += 1
+                if j > 2:
+                    break
+                print("Transaction #" + str(j) + " Sequence #: " + str(tcp.seq) + " Ack #: " + str(
+                    tcp.ack) + " Receive Window Size: " + str(tcp.win))
+
+
+def get_window_size(sport):
+    sender = '130.245.145.12'
+    receiver = '128.208.2.198'
+    f = open('assignment2.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    for ts, buffer in pcap:
+        eth = dpkt.ethernet.Ethernet(buffer)
+        ip = eth.data
+        tcp = ip.data
+
+        source_port = tcp.sport  # source port
+        dest_port = tcp.dport  # destination port
+        source_ip = inet_to_str(ip.src)
+        dest_ip = inet_to_str(ip.dst)
+
+        if source_ip == receiver and tcp.flags & dpkt.tcp.TH_FIN and sport == dest_port:
+            return tcp.win
 
 
 analysis_pcap_tcp('assignment2.pcap')
